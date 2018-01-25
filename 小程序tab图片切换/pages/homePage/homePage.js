d5573ba5df8dc2de801fb74f3779441912ca183a
wangqin <6734335@163.com>
2018-01-25 16:15:10 +0800

    小程序 列表选中切换图片

---
+++ 小程序tab图片切换/pages/homePage/homePage.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+  curIdx:null,
+  listInfo:[
+      {
+          title:'效果一',
+          imgUrl: '../../image/icon-1.png',
+          curUrl: '../../image/icon-1-cur.png',
+      },
+      {
+        title: '效果二',
+        imgUrl: '../../image/icon-2.png',
+        curUrl: '../../image/icon-2-cur.png',
+      },
+      {
+        title: '效果三',
+        imgUrl: '../../image/icon-3.png',
+        curUrl: '../../image/icon-3-cur.png',
+      },
+  ]
+  },
+   chooseImg: function (e) {
+     this.setData({
+       curIdx: e.currentTarget.dataset.current
+     })
+    //  console.log(e)
+    //  console.log(this.data.curIdx) 
+  }
+})
