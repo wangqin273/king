d66dba9a26d05ed2b7330a7ca6724b25b3b8d547
wangqin <6734335@163.com>
2018-02-09 18:28:41 +0800

    微信小程序之圆形进度条

--- game/components/circle/circle.js
+++ game/components/circle/circle.js
@@ -1,11 +1,8 @@
+/* components/circle/circle.js */
 Component({
   options: {
     multipleSlots: true // 在组件定义时的选项中启用多slot支持
   },
-  /**
-   * 组件的属性列表
-   * 用于组件自定义设置
-   */
   properties: {
     bg: {            // 属性名
       type: String,   // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
@@ -13,23 +10,15 @@ Component({
     },
     draw: {
       type: String,
-      value: 'draw' 
+      value: 'draw'
     },
+  },
 
-  },
-  /**
-   * 私有数据,组件的初始数据
-   * 可用于模版渲染
-   */
-  data: { 
+  data: { /*  私有数据，可用于模版渲染 */
     size: 0, /* 圆环盒子大小 size >= 2*x ( x 为canvas的绘制半径)*/
-    step:1,
-    num: 100  
+    step: 1,
+    num: 100
   },
-  /**
-   * 组件的方法列表
-   * 更新属性和数据的方法与更新页面数据的方法类似
-   */
   methods: {
     /*
      * 公有方法
@@ -37,7 +26,7 @@ Component({
     drawCircleBg: function (id, x, w) {
       // 设置圆环外面盒子大小 宽高都等于圆环直径
       this.setData({
-        size: 2*x
+        size: 2 * x   // 更新属性和数据的方法与更新页面数据的方法类似
       });
       // 使用 wx.createContext 获取绘图上下文 ctx  绘制背景圆环
       var ctx = wx.createCanvasContext(id)
@@ -54,7 +43,7 @@ Component({
       // 使用 wx.createContext 获取绘图上下文 context  绘制彩色进度条圆环
       var context = wx.createCanvasContext(id);
       // 设置渐变
-      var gradient = context.createLinearGradient(2*x, x, 0);
+      var gradient = context.createLinearGradient(2 * x, x, 0);
       gradient.addColorStop("0", "#2661DD");
       gradient.addColorStop("0.5", "#40ED94");
       gradient.addColorStop("1.0", "#5956CC");
@@ -66,18 +55,16 @@ Component({
       context.arc(x, x, x - w, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
       context.stroke();//对当前路径进行描边
       context.draw()
-
     },
-     /*
-     * 内部私有方法建议以下划线开头
-     * triggerEvent 用于触发事件
-     */
-    _runEvent(){
-      //触发回调
+    /* 内部私有方法建议以下划线开头 ，
+    自定义组件触发事件时，需要使用 triggerEvent 方法，指定事件名、detail对象和事件选项 */
+    _runEvent() {
+      //触发自定义组件事件
       this.triggerEvent("runEvent")
     }
   },
+
+  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
   onReady: function () {
-    
   }
 })
--- game/pages/index/index.js
+++ game/pages/index/index.js
@@ -7,13 +7,16 @@ Page({
     step:null,
     time:null,
     stepTimer:null,
-
-    count: 0,
+    
+ 
     txt: "正在匹配中...",
-    countTimer: null,
+    count: 0,//计数器，初始值为0
+    maxCount: 60, // 绘制一个圆环所需的步骤 
+    countTimer: null,//定时器，初始值为null
   },
   stepInterval:function(){
     // 设置倒计时 定时器
+  
     this.stepTimer = setInterval(() => {
       if (this.data.num >= 0) {
         this.data.step = this.data.num / 50;
@@ -35,11 +38,11 @@ Page({
   },
 
   countInterval: function () {
-    // 设置倒计时 定时器
+    // 设置倒计时 定时器 假设每隔100毫秒 count递增+1，当 count递增到两倍maxCount的时候刚好是一个圆环（ step 从0到2为一周），然后改变txt值并且清除定时器
     this.countTimer = setInterval(() => {   
-      if (this.data.count <= 100) {        
+      if (this.data.count <= 2 * this.data.maxCount) {        
         // 绘制彩色圆环进度条
-        this.circle.drawCircle('circle_draw1', 100, 8, this.data.count/50)
+        this.circle.drawCircle('circle_draw1', 100, 8, this.data.count / this.data.maxCount)
         this.data.count++;
         } else {
         this.setData({
@@ -63,25 +66,35 @@ Page({
     });
     // 重新开启倒计时
     this.stepInterval()
+
+    // 触发自定义组件事件
+    this._runEvent()
   },
 
   onLoad: function () {
   },
   onReady: function () {
+    /*倒计时*/
+
     // 获得circle组件
     this.circle = this.selectComponent("#circle");
     // 绘制背景圆环
     this.circle.drawCircleBg('circle_bg', 40, 4)
+    // 绘制彩色圆环
     this.stepInterval()
 
 
-
+    /*进度条 */
+   // 获得circle组件
     this.circle = this.selectComponent("#circle1");
     // 绘制背景圆环
-    this.circle.drawCircleBg('circle_bg1', 100, 8)
+    this.circle.drawCircleBg('circle_bg1', 100, 8);
+    this.circle.drawCircle('circle_draw1', 100, 8, 2);
+    // 绘制彩色圆环
+    // this.circle.drawCircle('circle_draw1', 100, 8, 2);
     this.countInterval()
 
-    this._runEvent()
+    
   },
   
   _runEvent() {
