e03d576cb9efa0b7969d69c2990672d701638124
wangqin273 <6734335@163.com>
2020-07-28 17:50:21 +0800

    美女图库

---
+++ gallery/js/component-header.js
@@ -0,0 +1,27 @@
+Vue.component("component-header", {
+	template: `
+	<div class="header">
+		<div class="header-home" @click="navHome()"><i class="iconfont icon-home"></i></div>
+		<div class="header-name" v-text="category_name"></div>
+		<div class="header-menu" @click="showMenu()"><i :class="['iconfont',menu_show?'icon-close':'icon-menu'] "></i></div>
+	</div>
+	`,
+	data: function() {
+		return {
+			menu_show: 0,
+		}
+	},
+	props: {
+		category_name: { 
+			default: '美图驿馆'
+		},
+	},
+	methods: {
+		navHome: function() {
+			location.href = "index.html"
+		},
+		showMenu: function() {
+			this.menu_show = !this.menu_show
+		},
+	}
+});
---
+++ gallery/js/component-list.js
@@ -0,0 +1,32 @@
+Vue.component("component-list", {
+	template: `
+	<ul class="albums-list">
+		<li v-for="item,i in albums">
+			<div class="photo-box" @click="navToDes(item)">
+				<div class="photo-title" v-text="item.title"></div>
+				<div class="lazy-img">
+					<img v-lazy="item.cover" src="item.cover" :alt="item.title" />
+				</div>
+				<div class="photo-time" v-text="item.time"></div>
+			</div>
+		</li>
+	</ul>
+	`,
+	data: function() {
+		return {
+
+		}
+	},
+	props: {
+		albums: Object,
+		column: {
+			type: Number,
+			default: 2
+		}
+	},
+	methods: {
+		navToDes: function(item) {
+			location.href = 'details.html?id=' + item.id
+		},
+	}
+});
